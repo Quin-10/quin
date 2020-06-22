7de19e079834d910f7ea92d91dbc1a182b21c7f7
Glitch (voltaic-empty-leaf) <none>
2020-06-22 23:17:05 +0000

    🌨❤️ Checkpoint ./Ban.js:16318365/444

--- Ban.js
+++ Ban.js
@@ -12,4 +12,14 @@ return message.channel.send(`**${message.author}**, You do not have the permissi
 return message.channel.send(`**${message.author}**, I am do not have perms to bam someone`)
   }
    
-const
+const target = message.mentions.members.first();
+    if(!target) {
+return message.channel.send(`${message.author.username} please mention the person who you want to ban.`)
+    }
+
+if(target.id === message.author.id) {
+return message.channel.send(`${message.author} are you like, dumb(jk) but you cannot ban yourself `)
+}
+    let embed = discord.MessageEmbed()
+    .setTitle("Action")
+  
