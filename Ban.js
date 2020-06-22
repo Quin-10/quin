e88cf409708dfa25058aafcc3634ba3d9d700984
Glitch (voltaic-empty-leaf) <none>
2020-06-22 23:04:51 +0000

    🚥🍕 Checkpoint ./Ban.js:16318365/243

--- Ban.js
+++ Ban.js
@@ -5,5 +5,11 @@ name: "ban lol",
   description: " bannig someone so XD",
   usage: "ban <@user> <reason>",
   run: async (client, message, args) => {
-if(message.member.hasPermission("BAN_MEMBERS")) {
-return message.channel.send(`**${message.author.username}**, You do not have the permission to ban a member. only the ser `)
+if(!message.member.hasPermission("BAN_MEMBERS")) {
+return message.channel.send(`**${message.author}**, You do not have the permission to ban a member.`)
+}
+  if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+return message.channel.send(`**${message.author}**, I am do not have perms to bam someone`)
+  }
+   
+const
