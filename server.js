dc10959261510ceb6e611798e7e04d84b9cbbb9c
Glitch (voltaic-empty-leaf) <none>
2020-06-23 03:57:16 +0000

    👯🎩 Checkpoint ./server.js:16318365/845

--- server.js
+++ server.js
@@ -224,7 +224,27 @@ bot.on('message', message => {
   if(key.some(word => message.content.includes(word)) ) { 
 const someone = bot.users.get(message.guild.members.random().id).id
   message.channel.send(`<@${someone}>`)
-}}
-       )
-       
+  }
+})
+
+const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+channel.send(exampleEmbed);    
+
 bot.login(TOKEN);
