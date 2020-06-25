a2f053915ba072d7f7524bec9360f5141ef34005
Glitch (voltaic-empty-leaf) <none>
2020-06-25 06:16:48 +0000

    📹🏔 Checkpoint ./server.js:16318365/891

--- server.js
+++ server.js
@@ -221,6 +221,31 @@ bot.on("message", message => {
   }
 });
 
+bot.on("message", message => {
+  if (message.content === '``E/color``') {
+    const exampleEmbed = new Discord.MessageEmbed()
+      .setColor("#FF5541")
+      .setTitle("Favorite colors")
+      .setAuthor("mr circle")
+      .setDescription("please chose your favorite color")
+      .setThumbnail(
+        "https://cdn.glitch.com/55924b02-3b4c-417c-80be-e9b40f99619e%2FCA5E5A4B-9855-44FE-8391-124EF36E8850.png?v=1592968948396"
+      );
+    message.channel.send(exampleEmbed)
+    message.react('🤙')
+      .then(() => message.react("🟠"))
+      .then(() => message.react("🟡"))
+      .then(() => message.react("🟢"))
+      .then(() => message.react("🔵"))
+      .then(() => message.react("🟣"))
+      .then(() => message.react("🟤"))
+      .then(() => message.react("⚫"))
+      .then(() => message.react("⚪"))
+      .catch(() => console.error("One of the emojis failed to react."));
+
+  }
+});
+
   
   
 bot.login(TOKEN);
