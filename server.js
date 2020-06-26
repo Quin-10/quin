13e4fcf7f53bf32fac176f86e3107e49176da13a
Glitch (voltaic-empty-leaf) <none>
2020-06-26 03:12:28 +0000

    🤑🐛 Checkpoint ./server.js:16318365/970

--- server.js
+++ server.js
@@ -257,14 +257,27 @@ message.channel.send("blah blah").then((message)=>{ message.edit('hello') })
   const infoWords = [ "@yahoo.com", "@gmail.com"];
   if (infoWords.some(word => message.content.includes(word))) {
 message.delete();
-  }
-      }
     message.member.send ('are you trying to leak your account?')
-    bot.on ("message", message => {
-    if (message.reply === 'no') {
-message.member.send ('ok')
-    }
-    })
+  }
+      })
+bot.on ("message", message => {
+if (message.content === 'spec'){
+        message.author.send("See or Change?");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content == "See") {
+                message.channel.send("You Want To See Someones Spec OK!");
+            } else if (message.content == "Change") {
+                message.channel.send("You Want To Change Your Spec OK!");
+            }
+        })}
+        
+
+  
+    
+    
+    
 
 
 
