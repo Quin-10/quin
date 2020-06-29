f2bddd2b237f78e10321dca7be476ad26f56e469
Glitch (voltaic-empty-leaf) <none>
2020-06-29 04:20:56 +0000

    🌾👴 Checkpoint ./server.js:16318365/182

--- server.js
+++ server.js
@@ -214,12 +214,21 @@ if(message.content.startsWith(`${prefix}mine`)) {
 bot.on("message", message => {
   if (message.content.startsWith(`${prefix}spoil`)) {
   var choices = [
-    `\n ||   01   |`,
+    `\n [**01**]`,
+`\n [**02**]`,
+`\n [**03**]`,
+`\n [**04**]`,
+`\n [**05**]`,
+`\n [**06**]`,
+`\n [**07**]`,
+`\n [**08**]`,
+`\n [**09**]`,
+`\n [**10**]`,
 
 
     ];
 var output = choices[Math.floor(Math.random() * choices.length)]; 
-    message.channel.send(`${message.author}**${output}**`);
+    message.channel.send(`${message.author}${output}`);
   }
 })
 bot.login(TOKEN);
