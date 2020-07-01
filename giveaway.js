2e7a1fbfab218cedf87c8bfb92f3e9bd5fc9339e
Glitch (voltaic-empty-leaf) <none>
2020-07-01 18:19:45 +0000

    💏🎸 Checkpoint ./giveaway.js:16318365/403

--- giveaway.js
+++ giveaway.js
@@ -1,3 +1,13 @@
 const {MessageEmbed2} = require('discord.js')
 const ms = require('ms');
-module.export
+module.exports={
+name: 'timer',
+  description:'create your timer',
+  usage: '<time> <channel> <prize>',
+  category: 'fun',
+  run: async(bot,message,args)=>{
+if(!args[0]) return message.channel.send(`you did not specify your time`)
+if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return message.channel.send(`You did not use `)
+    
+  }
+}
