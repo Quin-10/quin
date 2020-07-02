103ea562f2482b1035a337f144cb9421acf50b59
Glitch (voltaic-empty-leaf) <none>
2020-07-02 15:29:42 +0000

    🙎🌥 Checkpoint ./server.js:16318365/895

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ if(message.content.startsWith(`${prefix}say`)) {
  }})
 
 bot.on('message', message => {
-  if(message.content.startsWith(`${prefix}someone`)) {
+  if(message.content.startsWith(`${prefix}someboy`)) {
 const someone = (message.guild.members.random())  
   message.channel.send(`${someone}`)
   }
@@ -303,11 +303,11 @@ bot.user.setAvatar('https://cdn.glitch.com/55924b02-3b4c-417c-80be-e9b40f99619e%
 )
 bot.on('message', message => {
 if (message.content.startsWith(`${prefix}ban`)) {
+ const user = message.mentions.users.first();
   if (!message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) return message.channel.send('you don’t have those perms, you need to have the `KICK_MEMBERS` and `BAN_MEMBERS` permissions')
-    const user = message.mentions.users.first();
-  message.guild.members.ban(user);
-    }}
-)
+message.guild.members.ban(user);
+}
+})
 bot.on('message', message => {
   if (message.content.startsWith(`${prefix}stats1`)) {
   message.channel.send(`**Mr circle** server count: ${message.guilds.cache.size} servers!`);
