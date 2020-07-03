23b709a50175886355ce154b635266b5ad20295c
Glitch (voltaic-empty-leaf) <none>
2020-07-03 06:14:00 +0000

    👀💏 Checkpoint ./server.js:16318365/61

--- server.js
+++ server.js
@@ -291,7 +291,6 @@ message.channel.send(exampleEmbed1)
 })
 bot.on('message', message => {
   if (message.content.startsWith(`${prefix}announce`)) {
-if (!message.member.hasPermission(['ADMINISTRATOR'])) return
     var announce = message.content.split('').slice(1).join('');
     let channel = message.mentions.channels.first()
 channel.send(announce)
