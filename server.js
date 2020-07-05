cfc74d6406d88912bb34059064d3fea38892eb24
Glitch (voltaic-empty-leaf) <none>
2020-07-05 22:35:40 +0000

    🏀🐗 Checkpoint ./server.js:16318365/693

--- server.js
+++ server.js
@@ -358,5 +358,11 @@ message.guild.members.fetch().then(fetchedMembers => {
 })
 }
 })
-let memberCount =
+bot.on("message", message => {
+if (message.content.startsWith(`${prefix}channel`)) {
+message.guild.channels.create('new-channel', { type: 'text',  reason: 'New channel added for fun!' });
+}
+})
+bot.on("message", message => {
+if (message.content.startsWith(`${prefix}channel`)) {
 bot.login(TOKEN);
