a71fa477a0cf6263324c5d6022d39dc00a382f6a
Glitch (voltaic-empty-leaf) <none>
2020-07-10 23:01:07 +0000

    🏪🎓 Checkpoint ./giveaway.js:16318365/502

--- giveaway.js
+++ giveaway.js
@@ -1,4 +1,4 @@
-const {MessageEmbed} = require('discord.js')
+const {Message3Embed} = require('discord.js')
 const ms = require ('ms')
 module.exports={
 name: 'giveaway',
@@ -7,5 +7,13 @@ name: 'giveaway',
   category: 'fun',
   run: async(bot,message,args)=>{
 if(!args[0]) return message.channel.send('you did not say the time')
-    if(!args[0].endsWith("d")&&!args[0]
+    if(!args[0].endsWith("d")&&!args[0].endsWith("m")) return('you did not use the correct formatting')
+    if(isNaN(args[0][0])) return message.channel.send('that is not a number')
+    let channel = message.mentions.channels.first()
+    if(!channel) return message.channel.send('I can’t find that channel')
+    let prize = args.slice(2).join("")
+    if(!prize) return message.channel.send('you did not say the prize!')
+  }
+}
+
   
