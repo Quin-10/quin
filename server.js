dd88c905fe319d58332c3789b2e1584f3c1bd1eb
Glitch (voltaic-empty-leaf) <none>
2020-07-12 00:14:16 +0000

    👩🎸 Checkpoint ./server.js:16318365/1493

--- server.js
+++ server.js
@@ -480,46 +480,10 @@ const swearWords2 = ["19202993$329"]
   }
 }}
 )
-
 bot.on("message", message => {
-if (message.content.startsWith(`${prefix}give`)) {
-  const {Message3Embed} = require('discord.js')
-const ms = require ('ms')
-module.exports={
-name: 'giveaway',
-  description: 'create a giveaway',
-  usage: '<time> <channel> <prize>',
-  category: 'fun',
-  run: async(bot,message,args)=>{
-if(!args[0]) return message.channel.send('you did not say the time')
-    if(!args[0].endsWith("d")&&!args[0].endsWith("m")) return('you did not use the correct formatting')
-    if(isNaN(args[0][0])) return message.channel.send('that is not a number')
-    let channel = message.mentions.channels.first()
-    if(!channel) return message.channel.send('I can’t find that channel')
-    let prize = args.slice(2).join("")
-    if(!prize) return message.channel.send('you did not say the prize!')
-  }
+   if (message.content.startsWith(`${prefix}server`)) {
+if (message.content === `${prefix}server`) {
+	message.channel.send(`This server's name is: ${message.guild.name}`);
 }
-}})
-bot.on("message", async message => {
-if (message.content.startsWith(`${prefix}invite`)) {
-  const channel = message.mentions.channels.first()
-const invite = channel.createInvite({ temporary: true, reason: 'Just testing' });
-message.channel.send(invite)
-}
-})
-
-
-
-
-  bot.on('message', message => {
-	if (message.member.voice.channel.join()) {
-	const connection = message.member.voice.channel.join();
-    connection.play('https://youtu.be/IIyfO0cKGjY');
-	}
-});
-
-
-
 
 bot.login(TOKEN)
