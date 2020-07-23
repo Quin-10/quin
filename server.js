219ca9a82649c4ff48fd0fd43ae7baccb13d4c10
Glitch (voltaic-empty-leaf) <none>
2020-07-23 23:35:32 +0000

    🎭🛎 Checkpoint ./server.js:16318365/343

--- server.js
+++ server.js
@@ -622,8 +622,11 @@ bot.on('message', message => {
 bot.on('message', message => {
   
 
-if (command === 'hiya') {
-message.channel.send('hiyo')
+if(message.content.startsWith(`${prefix}slowmode`)) {
+    var time = message.content.split('').slice(1).join(' ')
+    if(!time) return message.reply('Please enter a time in seconds!')
+   message.channel.setRateLimitPerUser(time)
+     message.channel.send('Set the slowmode!')
   }
 })
 
