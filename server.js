2e54f8434113b0aaad5e2cb602818d7344dd55b1
Glitch (voltaic-empty-leaf) <none>
2020-07-27 07:38:22 +0000

    💈☃️ Checkpoint ./server.js:16318365/1708 ./Welcome.js:16318365/1342

--- server.js
+++ server.js
@@ -662,19 +662,8 @@ bot.on("message", function(message) {
     }, 1000 * 7);
   }
 });
-bot.on("message", message => {
-let channel = message.mentions.channels.first() //mentioned channel
-    
-    if(!channel) { //if channel is not mentioned
-      return message.channel.send("Please Mention the channel first")
-    }
-    
-    //Now we gonna use quick.db
-    
-    db.set(`welchannel_${message.guild.id}`, channel.id) //set id in var
-    
-    message.channel.send(`Welcome Channel is seted as ${channel}`) //send success message
-})
+
+
 
 
 
