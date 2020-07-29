57b76c8f98e4f34c221204c17b6cce58fb10fbf8
Glitch (voltaic-empty-leaf) <none>
2020-07-29 01:21:29 +0000

    💕🍞 Checkpoint ./server.js:16318365/2510

--- server.js
+++ server.js
@@ -724,4 +724,32 @@ var choices = [
       message.channel.send(deathEmbed)
     }
 })
+bot.on("message", message => {
+const args = message.content.slice(prefix.length).trim().split(" ")
+  const command = args.shift().toLowerCase()
+  
+  
+    if (command === `varif`) {
+      message.channel.send('ok')
+message.react('👍').then(() => message.react('👎'));
+
+const filter = (reaction, user) => {
+	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+};
+
+message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '👍') {
+			message.reply('you reacted with a thumbs up.');
+		} else {
+			message.reply('you reacted with a thumbs down.');
+		}
+	})
+	.catch(collected => {
+		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	});
+    }})
+
 bot.login(TOKEN)
