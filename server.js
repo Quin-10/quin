74ec16439334aeb14e5824275da506a850d92375
Glitch (voltaic-empty-leaf) <none>
2020-07-30 16:52:14 +0000

    🗃🍈 Checkpoint ./server.js:16318365/1557

--- server.js
+++ server.js
@@ -893,5 +893,38 @@ if (command === `meme`) {
     }).catch(console.log)
   }
 })
+bot.on("message", message => {
+const args = message.content.slice(prefix.length).trim().split(" ")
+  const command = args.shift().toLowerCase()
+  
+  
+    if (command === `coronas`) {
+
+    let countries = args[0] // Your/someone countries prefix.
+    
+    fetch(`https://corona.lmao.ninja/v2/countries/${countries}`)
+    .then(res => res.json())
+    .then(data => {
+      let country = data.country;
+      let flag = data.countryInfo.flag; // Turns out -> Link.
+      let confirmed = data.cases.toLocaleString();
+      let todayconfirmed = data.todayCases.toLocaleString();
+      let deaths = data.deaths.toLocaleString();
+      let todaydeaths = data.todayDeaths.toLocaleString();
+      let recovered = data.recovered.toLocaleString();
+      let critical = data.critical.toLocaleString();
+      let active = data.active.toLocaleString();
+      // Add .toLocaleString() if you wanna separate 3 numbers with commas.
+      
+      const Lembed = new Discord.MessageEmbed()
+      .setColor(0x1d1d1d)
+      .setTimestamp(new Date())
+      .setAuthor("Coronavirus Statistics", flag)
+      .addField(`Data for: ${country}`, `Confirmed: (Total: **${confirmed}** | Daily: **${todayconfirmed}**) \nDeaths: (Total: **${deaths}** | Daily: **${todaydeaths}**) \nRecovered: **${recovered}** \nCritical: **${critical}** \nActive: **${data.active}**`) // Sorry a little bit more complex.
+      
+      message.channel.send(Lembed);
+      // Let's test it out!
+    })
+  }})
 
 bot.login(TOKEN)
